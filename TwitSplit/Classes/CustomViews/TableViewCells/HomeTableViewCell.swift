db2ae0d9a7e5fcd1542f9a93cfc7873a97339eb8
Massive Infinity - Hizami <hizami@massiveinfinity.com>
2019-01-19 14:20:15 +0800

    added tabbar image, animated splash screen, sample tableView data

---
+++ TwitSplit/Classes/CustomViews/TableViewCells/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  TwitSplit
+//
+//  Created by Massive Infinity on 19/1/19.
+//  Copyright © 2019 Hizami Rashid. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
